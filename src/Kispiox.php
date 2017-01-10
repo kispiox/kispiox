66197142700c6af2866496a9456a06fada1a6e35
Matt Ferris <matt@bueller.ca>
2017-01-09 23:30:02 -0800

    Added boilerplate and start of first-run/demo files

--- src/Kispiox.php
+++ src/Kispiox.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * Kispiox - A lightweight application framework
+* www.bueller.ca/kispiox
+*
+* Kispiox.php
+* @copyright Copyright (c) 2016 Matt Ferris
+* @author Matt Ferris <matt@bueller.ca>
+*
+* Licensed under BSD 2-clause license
+* www.bueller.ca/kispiox/license
+*/
+
 namespace Kispiox;
 
 use MattFerris\Configuration\Configuration;
